799fb4593839212d5e43590ea64b94c7605532a5
King.Lao <chiune@gmail.com>
2023-03-02 13:10:10 +0000

    clean up

---
+++ project/table-service/src/main/java/com/casino/tableservice/Hello.java
@@ -0,0 +1,11 @@
+package com.casino.tableservice;
+
+import java.util.Date;
+
+public class Hello {
+    public static void main(String[] args) {
+        Date currentDate = new Date();
+        System.out.println(
+                "Hello, world! " + currentDate.getTime());
+    }
+}
